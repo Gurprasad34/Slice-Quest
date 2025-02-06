701683560752bcdb1aadf2c93a6b816e4dce5bb5
your-username <marmolejo.ricardo@gmail.com>
2025-02-06 16:03:09 -0500

    package jason & vite.config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [
+    tailwindcss(),
+  ],
   server: {
+    
     port: 3000,
     open: true,
     proxy: {
