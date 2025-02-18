424dc85ac49cd39a686c9890c6475505e6695449
bryanesslinger <bryanessl@gmail.com>
2025-02-17 20:21:08 -0500

    changing port to 3000 in server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,7 +8,7 @@ import { fileURLToPath } from 'url';
 import { seedAll } from './seeds/index.js';
 
 const app = express();
-const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 3001;  // Ensure PORT is parsed correctly
+const PORT = process.env.PORT || 3000;
 const forceDatabaseRefresh = false;
 
 // Log the PORT to check its value
